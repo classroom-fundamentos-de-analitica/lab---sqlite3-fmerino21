9f5cd799784834afa65fb1c71c60f941beaeb455
merinsr21 <80010472+fmerino21@users.noreply.github.com>
2022-10-03 09:54:52 -0500

    8

--- pregunta_08.sql
+++ pregunta_08.sql
@@ -41,6 +41,6 @@
 --
 --  >>> Escriba su codigo a partir de este punto <<<
 --
-SELECT SUBSTR(c23, 1, 4) AS YEAR, avg(c21)
+SELECT strftime('%Y', c23) AS YEAR, avg(c21)
 FROM tbl2
-GROUP BY SUBSTR(c23, 1, 4)
+GROUP BY strftime('%Y', c23)
